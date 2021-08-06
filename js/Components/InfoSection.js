1db1a9be50722992a19a3620e74c1a8740e853b9
mghazal1024 <mghazal1024@gmail.com>
2021-08-06 19:03:28 +0200

    commit

---
+++ js/Components/InfoSection.js
@@ -0,0 +1,23 @@
+const InfoSection = (portrait, name, city, country, tagline, pTags) => {
+    let infoSection = `
+        <section class="photographer">
+            <div class="photographer__info">
+                <div class="photographer__info-contact">
+                    <h1>${name}</h1>
+                    <button>Contactez-moi</button>
+                </div>
+                <h2 class="primary-color">${city}, ${country}</h2>
+                <p>${tagline}</p>
+                <ul class="photographer__tags">
+                    ${pTags.join(' ')}
+                </ul>
+            </div>
+            <div class="photographer__portrait">
+                <img src="/images/${portrait}" alt=${name} />
+            </div>
+        </section>
+        `
+    return infoSection;
+}
+
+export default InfoSection
