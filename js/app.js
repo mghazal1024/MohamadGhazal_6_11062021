e631c5e3a9de4b86cb12b635832582a2f5f26a38
mghazal1024 <mghazal1024@gmail.com>
2021-08-17 12:20:40 +0200

    added header content button on scroll

--- js/app.js
+++ js/app.js
@@ -4,6 +4,21 @@ import PhotographerCard from './Helpers/PhotographerCard.js';
 import HandleMainNavFilter from './Helpers/HandleMainNavFilter.js';
 import PhotographerPage from './Helpers/PhotographerPage.js';
 
+
+// DOM ELEMENTS
+const contentButton = document.querySelector('.content-btn');
+
+
+
+window.addEventListener('scroll', () => {
+    let currentScrollPosition = window.pageYOffset;
+    if(currentScrollPosition > 50) {
+        contentButton.classList.add('show');
+    } else {
+        contentButton.classList.remove('show');
+    }
+})
+
 //Load the api and use it
 LoadData().then( (data) => {
     // Check if page is a photographer page to create the page dynamically, If not it renders the homepage
